8b5030bb0883f6ca6bd6c2cb387862313a96b5a0
Nitish Kumar <nk98721000@gmail.com>
2023-03-10 17:49:41 +0530

    admission card fixed

--- js/root.js
+++ js/root.js
@@ -1,3 +1,13 @@
-function closeAdmissionCard() {
-    document.getElementById('admissionCard').style.display = 'none';
+let whatsNewCardOpenState = false;
+
+
+function admissionCardListener() {
+    const card = document.getElementById('admissionCard');
+    if (whatsNewCardOpenState) {
+        card.style.animation = 'slideToRight 0.2s linear forwards';
+        whatsNewCardOpenState = false;
+    } else {
+        card.style.animation = 'slideToLeft 0.2s linear forwards';
+        whatsNewCardOpenState = true;
+    }
 }
