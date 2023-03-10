a2ca601a209b4b23e5b6f3a438edcb2d51d926e6
Shivang <harshsaini5656@gmail.com>
2023-03-10 14:18:42 +0530

    Updated  Team

---
+++ js/swiper.js
@@ -0,0 +1,37 @@
+// var swiper = new Swiper(".mySwiper", {
+//     slidesPerView: 4,
+//     // centeredSlides: true,
+//     spaceBetween: 50,
+//     grabCursor: true,
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//   });
+// var swiper = new Swiper(".mySwiper", {
+//     slidesPerView: 1,
+//     spaceBetween: 30,
+//     loop: true,
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//   });
+
+// var swiper = new Swiper(".mySwiper", {
+//     slidesPerView: 1,
+//     spaceBetween: 30,
+//     loop: true,
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//   });
