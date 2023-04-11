ddbbe6a6b4ff4de16872b7ce9031cc1136089516
Himanshu Chaudhary <him.chaudhary212@gmail.com>
2023-04-11 12:16:11 -0700

    iqac page

--- js/root.js
+++ js/root.js
@@ -11,3 +11,19 @@ function admissionCardListener() {
         whatsNewCardOpenState = true;
     }
 }
+
+function listToggle(id){
+    let listElement = document.getElementById(id);
+    if(listElement.dataset.toggle === 'off'){
+        listElement.childNodes[1].style.display = 'block';
+        listElement.style.setProperty('--toggle-icon',"'~ '");
+        listElement.style.setProperty('--icon-color',"green");
+        listElement.dataset.toggle ='on';
+    }
+    else {
+        listElement.childNodes[1].style.display = 'none';
+        listElement.style.setProperty('--toggle-icon',"'+ '");
+        listElement.style.setProperty('--icon-color',"rgb(33, 33, 156)");
+        listElement.dataset.toggle = 'off';
+    }
+}
